d44e4650feffe1c085881ef85ae6cbd28feab464
frieddeli <130236946+frieddeli@users.noreply.github.com>
2025-01-06 01:17:19 +0800

    Implement brightness retrieval and keyboard hotkeys for adjustment

--- modules.py
+++ modules.py
@@ -4,6 +4,8 @@ import logging
 from monitorcontrol import get_monitors
 
 
+
+
 #function to retrieve the monitors connected to the computer
 def RetrieveMonitors():
     monitors = get_monitors()
@@ -14,11 +16,10 @@ def RetrieveMonitors():
 def ChangeBrightness(monitor, brightness):
     with monitor:
     #query the user for the desired brightness level
-        brightness = input(f"Enter the desired brightness level (0-100) for monitor {monitor}: ")
-        brightness = int(brightness)
         try:
             monitor.set_luminance(brightness)
         except Exception as e:
             print(f"  Error interacting with monitor: {e}")
     print(f"  Set brightness to: {brightness}")
 
+                
--- monitor.py
+++ monitor.py
@@ -10,43 +10,78 @@ from modules import * # import all functions from modules.py
 monitor_brightness = {} # init a dict to hold the values 
 
 
+#define a function to poll the brightness of each monitor
+def RetrieveBrightness():
+    for monitor in get_monitors():
+        print(f"Monitor: {monitor}")
+        with monitor:  # Open a context for the monitor
+            try:
+                # Get the current brightness level
+                brightness = monitor.get_luminance()
+                print(f"  Current Brightness: {brightness}")
+                #round the brightness to the nearest 10
+                brightness = round(brightness, -1)
+                monitor_brightness[monitor] = brightness # add the monitor and its brightness to the dictionary
+        
+            except Exception as e:
+                print(f"  Error interacting with monitor: {e}")
+                
+                
+#poll the keyboard for keypresses of the hotkey
 
+def RetrieveKeyboard():
+    try:
+        #increase brightness 
+        if keyboard.is_pressed('page up'):
+            print("Hotkey pressed up")
+            
+            for monitor in monitor_brightness:
+                TargetBrightness = monitor_brightness[monitor] + 10
+                
+                if TargetBrightness > 100:
+                    print("  Maximum brightness reached")
+                    TargetBrightness = 100
+                    continue
+          
+                ChangeBrightness(monitor, TargetBrightness) # increase the brightness by 10
+                monitor_brightness[monitor] += 10
+            time.sleep(0.1)
+            return 
+        #decrease brightness
+        if keyboard.is_pressed('page down'):
+            print("Hotkey pressed down")
+            
+            #sanity check for the brightness level
+            for monitor in monitor_brightness:
+                
+                TargetBrightness = monitor_brightness[monitor] - 10
+                
+                if TargetBrightness < 0:
+                    print("  Monitor brightness too low")
+                    TargetBrightness = 0
+                    continue
+            
+                ChangeBrightness(monitor, TargetBrightness) # decrease the brightness by 10
+                monitor_brightness[monitor] -= 10
+            time.sleep(0.1)
+            return
+            
+    except Exception as e:
+        print(f"Error polling keyboard: {e}")
+        return
+    time.sleep(0.1)
+
+                    
+                    
 
 # SHOW monitors
 print(f"Detected monitors: {RetrieveMonitors()}")
 
-for monitor in get_monitors():
-    print(f"Monitor: {monitor}")
-    with monitor:  # Open a context for the monitor
-        try:
-            # Get the current brightness level
-            brightness = monitor.get_luminance()
-            print(f"  Current Brightness: {brightness}")
-            monitor_brightness[monitor] = brightness # add the monitor and its brightness to the dictionary
-    
-        except Exception as e:
-            print(f"  Error interacting with monitor: {e}")\
-                
-                
-#poll the keyboard for keypresses of the hotkey
-while True:
-    try:
-        #increase brightness 
-        if keyboard.is_pressed('crtl+arrow-up'):
-            print("Hotkey pressed")
-            for monitor in monitor_brightness:
-                ChangeBrightness(monitor, monitor_brightness[monitor]+10) # increase the brightness by 10
-            time.sleep(1)
-        #decrease brightness
-        if keyboard.is_pressed('function+arrow-down'):
-            print("Hotkey pressed")
-            for monitor in monitor_brightness:
-                ChangeBrightness(monitor, monitor_brightness[monitor]-10) # decrease the brightness by 10
-            time.sleep(1)
-            
-    except Exception as e:
-        print(f"Error polling keyboard: {e}")
-        break
-    time.sleep(0.1)
 
+
+#main loop
+while True:
+    RetrieveBrightness()
+    RetrieveKeyboard()
+    time.sleep(0.5)
 
